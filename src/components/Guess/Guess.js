bd729f6644ceddbd5be0c50e2c2bed934f44c9f0
Igor Atilar <igoratilar10@gmail.com>
2023-10-29 18:54:15 -0300

    4# exercise

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,23 @@
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value = '' }) {
+function Cell({ letter = '', status = '' }) {
+  const className = status ? `cell ${status}` : `cell`;
+
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value = '', answer = '' }) {
+  const result = checkGuess(value, answer) || [];
+
   return (
     <p className='guess'>
       {range(5).map((num) => (
-        <span key={num} className='cell'>
-          {value[num]}
-        </span>
+        <Cell
+          key={num}
+          letter={result[num]?.letter}
+          status={result[num]?.status}
+        />
       ))}
     </p>
   );
